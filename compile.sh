fb8ca468c66173cd507702022e0b8e307d770b38
Francesco Sonnessa <80450457+S0nFra@users.noreply.github.com>
2023-11-28 16:31:36 +0100

    Update compile.sh (TO TEST)

--- compile.sh
+++ compile.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# Run PyInstaller to create the executable
 pyinstaller --noconsole --onefile \
-    --add-data ".\icon\*.png:img" \
-    .\app.py
+    --add-data "./icon/*.png:img" \
+    ./app.py
+
+# Get the full path of the script
+ws=$(readlink -f "$0")
+
+# Check if the destination folder exists
+if [ ! -d "$ws/dist/icon" ]; then
+    echo
+    echo "Manual copy"
+
+    # Create the destination folder
+    mkdir -p "$ws/dist/icon"
+
+    # Copy files from the source folder to the destination folder
+    cp -r "$ws/icon"/* "$ws/dist/icon"
+fi
