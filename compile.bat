728b43e1141f66ab16714cda2fa273896bc8b3b7
Francesco Sonnessa <80450457+S0nFra@users.noreply.github.com>
2023-11-29 11:38:31 +0100

    no --add-data in compile files

--- compile.bat
+++ compile.bat
@@ -1,3 +1,8 @@
-pyinstaller --noconsole --onefile ^
-    --add-data ".\icon\*.png;img" ^
-    .\app.py
+pyinstaller --noconsole --onefile .\app.py
+
+if not exist "%cd%\dist\icon" (
+    echo:
+    echo # Manual copy
+    mkdir "%cd%\dist\icon"
+    copy "%cd%\icon\*" "%cd%\dist\icon"
+)
--- compile.sh
+++ compile.sh
@@ -1,3 +1,16 @@
-pyinstaller --noconsole --onefile \
-    --add-data ".\icon\*.png:img" \
-    .\app.py
+pyinstaller --noconsole --onefile .\app.py
+
+# Get the full path of the script
+ws=$(readlink -f "$0")
+
+# Check if the destination folder exists
+if [ ! -d "$ws/dist/icon" ]; then
+    echo
+    echo "Manual copy"
+
+    # Create the destination folder
+    mkdir -p "$ws/dist/icon"
+
+    # Copy files from the source folder to the destination folder
+    cp -r "$ws/icon"/* "$ws/dist/icon"
+fi
