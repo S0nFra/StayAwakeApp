f9acacfe96a79ff107918a4a7d08c8bc28cc1db4
Francesco Sonnessa <80450457+S0nFra@users.noreply.github.com>
2023-11-29 11:06:27 +0100

    compile files moved (again)

--- compile.bat
+++ compile.bat
@@ -1,10 +1,3 @@
 pyinstaller --noconsole --onefile ^
     --add-data "icon\*.png;." ^
     .\app.py
-
-if not exist "%cd%\dist\icon" (
-    echo:
-    echo # Manual copy
-    mkdir "%cd%\dist\icon"
-    copy "%cd%\icon\*" "%cd%\dist\icon"
-)
