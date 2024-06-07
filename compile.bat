a0b74522a71a390b5f222d0fc887c4e0b43832ae
Francesco Sonnessa <sonnessa75@gmail.com>
2024-06-07 09:57:40 +0200

    Renamed app.py to StayAwakeApp.py

--- compile.bat
+++ compile.bat
@@ -1,8 +1,1 @@
-pyinstaller --noconsole --onefile .\app.py
-
-if not exist "%cd%\dist\icon" (
-    echo:
-    echo # Manual copy
-    mkdir "%cd%\dist\icon"
-    copy "%cd%\icon\*" "%cd%\dist\icon"
-)
+pyinstaller --noconsole --onefile .\StayAwakeApp.py
--- compile.sh
+++ compile.sh
@@ -2,18 +2,3 @@
 
 # Run PyInstaller to create the executable
 pyinstaller --noconsole --onefile .\app.py
-
-# Get the full path of the script
-ws=$(readlink -f "$0")
-
-# Check if the destination folder exists
-if [ ! -d "$ws/dist/icon" ]; then
-    echo
-    echo "Manual copy"
-
-    # Create the destination folder
-    mkdir -p "$ws/dist/icon"
-
-    # Copy files from the source folder to the destination folder
-    cp -r "$ws/icon"/* "$ws/dist/icon"
-fi
